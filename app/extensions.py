4e6bd45d872802f2b428e0f2e50958bd2b56f67d
miguel8990 <miguel88leonel.88@gmail.com>
2025-12-16 10:38:40 -0300

    estilo do botão

--- app/extensions.py
+++ app/extensions.py
@@ -6,7 +6,19 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_socketio import SocketIO
 from flask_redis import FlaskRedis
+from flask import request
 # Instanciamos tudo aqui, mas sem ligar ao 'app' ainda
+
+def get_real_ip():
+    # Tenta pegar o cabeçalho do Cloudflare, se não existir, usa o padrão
+    if request:
+        return request.headers.get("CF-Connecting-IP") or get_remote_address()
+    return "127.0.0.1"
+
+
+
+
+
 db = SQLAlchemy()
 ma = Marshmallow()
 jwt = JWTManager()
